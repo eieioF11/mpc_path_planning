bcef5168352dce1bfa3e92d84222bcaf1f9806e5
F11 <eieioeiji0501@gmail.com>
2023-04-30 15:54:43 +0900

    [add]Assign grid path as initial value

--- mpc_test.py
+++ mpc_test.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 
 
 class mpc_config(object):
-    horizon = 5  # ホライゾン長さ
-    dt = 0.1  # 離散化ステップ
+    horizon = 200  # ホライゾン長さ
+    dt = 0.01  # 離散化ステップ
     # 機体スペック
     max_velocity = 1.0  # [m/s]
     max_angular = 1.0  # [m/s]
@@ -62,6 +62,33 @@ class OmniDirectional(Kinematic):
         position_next = self.position_ + config_.dt * velocity_next
         state_next = casadi.vertcat(velocity_next, position_next)
         return casadi.Function("OmniDirectional", [self.state_, self.control_], [state_next])
+
+# 制約条件
+def guard_circle_subject(xy,center,size,comp):#円の制約
+    dx = casadi.times(casadi.MX(xy) - casadi.MX(center),casadi.vertcat(casadi.MX({2./size[0],2./size[1]})))
+    lh = dx**2
+    if comp == "<" or comp == "keep in":
+        return lh < 1
+    if comp == ">" or comp == "keep out":
+        return lh > 1
+    if comp == "<=":
+        return lh <= 1
+    if comp == ">=":
+        return lh >= 1
+    return lh
+
+def guard_rect_subject(xy,center,size,comp):#四角の制約
+    dx = casadi.times(casadi.MX(xy) - casadi.MX(center),casadi.vertcat(casadi.MX({1./size[0],1./size[1]})))
+    lh = casadi.MX(abs(dx[0]+dx[1])+abs(dx[0]-dx[1]))
+    if comp == "<" or comp == "keep in":
+        return lh < 1
+    if comp == ">" or comp == "keep out":
+        return lh > 1
+    if comp == "<=":
+        return lh <= 1
+    if comp == ">=":
+        return lh >= 1
+    return lh
 
 # 最適化変数
 u_sol_ = casadi.DM.zeros(3, config_.horizon)
@@ -147,10 +174,33 @@ grid=np.array([
 ])
 map_resolution = 1
 origin = [0,0]
+def path_length(path):
+    length=0.
+    for i in range(len(path)-1):
+        diff = path[i+1]-path[i]
+        length+=np.sqrt(abs(diff[0])+abs(diff[1]))
+    return length
+#変換
 def conversion_grid_pos(map_pos):
     return [int(map_pos[0]/map_resolution+origin[0]),int(map_pos[1]/map_resolution+origin[1])]
 def conversion_map_pos(grid_pos):
     return [(grid_pos[0]-origin[0])*map_resolution,(grid_pos[1]-origin[1])*map_resolution]
+#線形補間
+def lerp(path,t):
+    if t<0.0:
+        t=0
+    passed_len = 0.
+    p=np.array([path[-1][0],path[-1][1]])
+    for i in range(len(path)-1):
+        diff = path[i+1]-path[i]
+        l=np.sqrt(abs(diff[0])+abs(diff[1]))
+        if (passed_len+l) > t:
+            t-=passed_len
+            t/=l
+            p=path[i]+diff*t
+            return p
+        passed_len+=l
+    return p
 
 grid_path_planner = a_star.PathPlanner(grid,False)
 init,path=grid_path_planner.a_star(np.array([int(current_position[0]),int(current_position[1])]),np.array([int(target_position[0]),int(target_position[1])]))
@@ -162,9 +212,12 @@ if init!=-1:
     plt.plot(path[:,1],path[:,0])
     plt.show()
 # 初期値代入
+path_len = path_length(path)
+path_pos = casadi.DM.zeros(6)
+init_path_result = []
 for i in range(config_.horizon):
     if init!=-1:
-        path_len = len(path)
+        #grid path
         yaw_t = i / (config_.horizon -1.0)
         step = path_len / (config_.horizon -1.0)
 
@@ -175,15 +228,35 @@ for i in range(config_.horizon):
         if glen > path_len:
             glen = path_len
 
-        #xy=conversion_map_pos(xy[0],xy[1])
+        grid_path_pos = lerp(path,glen)
+        print(grid_path_pos,glen)
+        grid_path_pos=conversion_map_pos(grid_path_pos)
+        yaw = current_position[2] + (target_position[2]-current_position[2])*yaw_t
+        init_path_result.append([grid_path_pos[0],grid_path_pos[1],yaw])
+        pos = casadi.DM([grid_path_pos[0],grid_path_pos[1],yaw])
+        vel = (pos-x_init_[3:6,i])/config_.dt
+        print(vel,pos)
+        path_pos = casadi.vertcat(vel,pos)
+        u_init[:,i] = vel
+        x_init_[:,i+1] = path_pos
     else :
+        #線形補間
         print('error')
         u_init[:,i] = casadi.DM.zeros(3)
         x_init_[:,i+1]=dm_current_state_ + (dm_X_target - dm_current_state_)*(i/(config_.horizon-1))
         print(x_init_[:,i])
 
+init_path_result=np.array(init_path_result)
+
 opti_.set_initial(U,u_init)
 opti_.set_initial(X,x_init_)
+
+# end_diff = (dm_X_target - x_init_[:,config_.horizon-1])[3:5]
+# print("end_diff")
+# end_diff_vec = np.array(end_diff.T)[0]
+# print(end_diff_vec)
+# if np.linalg.norm(end_diff_vec, ord=0)>0.5:
+#     dm_X_target = x_init_[:,config_.horizon-1]
 
 opti_.set_value(X_target_,dm_X_target)
 
@@ -194,3 +267,20 @@ sol = opti_.solve()
 print(f'評価関数：{sol.value(obj)}')
 print(f'X: {sol.value(X)}')
 print(f'U: {sol.value(U)}')
+# 解をプロット
+x1_opt = np.array(sol.value(X)[0::6])
+x2_opt = np.array(sol.value(X)[1::6])
+x3_opt = np.array(sol.value(X)[2::6])
+u1_opt  = np.array(sol.value(X)[3::6])
+u2_opt  = np.array(sol.value(X)[4::6])
+u3_opt  = np.array(sol.value(X)[5::6])
+print(u1_opt[0],u2_opt[0],u3_opt[0])
+plt.figure(1)
+plt.clf()
+plt.grid()
+plt.xlabel('x')
+plt.ylabel('y')
+plt.plot(init_path_result[:,0], init_path_result[:,1], '-')
+plt.plot(x1_opt, x2_opt, '.')
+plt.grid()
+plt.show()
